98fa140b0baa1d31457949ab664d4b38c3a7f759
nativeTiger <santakumal41@gmail.com>
2022-01-18 23:42:34 +0545

    Added the pagination and bug fixes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,34 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ProjectList from "./ProjectList";
 import Notification from "./Notification";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { Redirect } from "react-router-dom";
-class Dashboard extends Component {
-  render() {
-    const { projects, authId } = this.props;
-    if (!authId) return <Redirect to="/signin" />;
-    return (
-      <main className="main">
-        <section className="home section" id="home">
-          <div className="home__container container">
-            <div className="home__content">
-              <div className="home__projectlist">
-                <h2 className="home__projectlist__title">Projects</h2>
-                <ProjectList projects={projects} />
-              </div>
-              <div className="home__notification">
-                <h2 className="home__notification__title">Notifications</h2>
-                <Notification />
-              </div>
+import Pagination from "./Pagination";
+
+const Dashboard = ({ projects, authId }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [projectsPerPage] = useState(2);
+  const indexOfLastProjects = currentPage * projectsPerPage;
+  const indexOfFirstProjects = indexOfLastProjects - projectsPerPage;
+  let currentProjects, totalProjects;
+  if (projects) {
+    currentProjects = projects.slice(indexOfFirstProjects, indexOfLastProjects);
+    totalProjects = projects.length;
+  }
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  if (!authId) return <Redirect to="/signin" />;
+  return (
+    <main className="main">
+      <section className="home section" id="home">
+        <div className="home__container container">
+          <div className="home__content">
+            <div className="home__projectlist">
+              <h2 className="home__projectlist__title">Projects</h2>
+              <ProjectList projects={currentProjects} />
+              <Pagination
+                projectsPerPage={projectsPerPage}
+                totalProjects={totalProjects}
+                paginate={paginate}
+              />
+            </div>
+            <div className="home__notification">
+              <h2 className="home__notification__title">Notifications</h2>
+              <Notification />
             </div>
           </div>
-        </section>
-      </main>
-    );
-  }
-}
+        </div>
+      </section>
+    </main>
+  );
+};
 const mapStatesToProps = ({ firestore, firebase }) => {
   return {
     projects: firestore.ordered.projects,
---
+++ src/components/dashboard/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = ({ projectsPerPage, totalProjects, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalProjects / projectsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="pagination__item">
+            <button
+              onClick={() => paginate(number)}
+              className="pagination__link"
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Pagination;
